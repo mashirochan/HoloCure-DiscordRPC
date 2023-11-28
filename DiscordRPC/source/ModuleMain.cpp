8f9da3e614db85e9d494d1c82bffda7d303ca2f3
Mashiro-chan <11561006+mashirochan@users.noreply.github.com>
2023-11-27 21:40:57 -0500

    Add mod info export

--- DiscordRPC/source/ModuleMain.cpp
+++ DiscordRPC/source/ModuleMain.cpp
@@ -10,6 +10,51 @@ using namespace YYTK;
 static YYTKInterface* g_ModuleInterface = nullptr;
 
 /*
+		Mod Info
+*/
+static const char* mod_name = "Discord RPC";
+static int version_major = 1;
+static int version_minor = 5;
+static int version_patch = 1;
+static const char* mod_repo = "https://github.com/mashirochan/HoloCure-DiscordRPC/releases/latest";
+static const char* mod_icon = "iVBORw0KGgoAAAANSUhEUgAAAD4AAAAvCAYAAAC7S3l6AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAANdMAADXTAQwhQ3cAAAHgSURBVGhD7ZpBTsQwDEWn7JE4AhyBEwA3YAkrYDHsuQlrNrBjyRXgBlyBEyDgAB0n41RRpkkT2w2TJk+q7I4qx9/ONErUru/7lSTX939DwNenww5dEpKxXNjC7eRcJIW7cGOThIcScqEmOPcY0cIhkUswb9u7veUBivCIfpAo4SnV3wdiZsABWi8g+hbdYoCcX9D1MikceEZbEjdovQSFlzbFbaZyj+n4IvEKL7nbhpCG1nGbJXTb4NPSOm5YUrcNY5paxxVL7LbB1dY6XhuD8CVPc4OtsdqOD/txbsd9e2BqXOl4BhNXdxyCHSlLJbTxjzkUcJGOZwNa1UnSMNW/0SYTmcgHWhGY4vXxWZb/OCR6ju4kCaK+0JJgCedOOw4w9jG6JKp9q9crnLs8lIjSzOr4fxaNO3aWqZ6SZK5isoVPJUoRMkdMl+5q/StWYXd54yYoHc9GVHhJtHW8Nprw2mjCKeByc7K9y8odd2eoj56o6+Oc6+wYUuOpOPaZ2zuYM32TiK/6AoU4hdif6A8w4v5APH3MtvPxDydZXwGkkMxt5z+uHphbQE58erwvN3w4+sWVo1iJY1yEno/6zk0xNc1yCFdI5REt3DA2cC7RBpEclHDqpXZ27m+5Lt7Y/WoDbDepTNwiK2cAAAAASUVORK5CYII=";
+
+struct ModInfo {
+	const char* mod_name;
+	int version_major;
+	int version_minor;
+	int version_patch;
+	const char* mod_repo;
+	const char* mod_icon;
+};
+
+EXPORTED inline void aurie_get_mod_info(ModInfo* info) {
+	info->mod_name = mod_name;
+	info->version_major = version_major;
+	info->version_minor = version_minor;
+	info->version_patch = version_patch;
+	info->mod_repo = mod_repo;
+	info->mod_icon = mod_icon;
+}
+
+/*
+		Inline Functions
+*/
+template <typename... TArgs>
+inline static void Print(CmColor Color, std::string_view Format, TArgs&&... Args) {
+	g_ModuleInterface->Print(Color, Format, Args...);
+}
+
+template <typename... TArgs>
+inline static void PrintError(std::string_view Filepath, int Line, std::string_view Format, TArgs&&... Args) {
+	g_ModuleInterface->PrintError(Filepath, Line, Format, Args...);
+}
+
+inline static RValue CallBuiltin(const char* FunctionName, std::vector<RValue> Arguments) {
+	return g_ModuleInterface->CallBuiltin(FunctionName, Arguments);
+}
+
+/*
 		Discord SDK Variables
 */
 constexpr auto APPLICATION_ID = 1151654619244666951;
@@ -20,7 +65,7 @@ int UUUUUUUChance = 10;
 std::map<double, std::pair<std::string, std::string>> stageMap;
 
 static void LogProblemsFunction(discord::LogLevel level, std::string message) {
-	g_ModuleInterface->PrintError(__FILE__, __LINE__, "Discord: %d - %s", static_cast<int>(level), message.c_str());
+	PrintError(__FILE__, __LINE__, "Discord: %d - %s", static_cast<int>(level), message.c_str());
 }
 
 std::random_device rd;
@@ -67,10 +112,19 @@ static std::string ConvertToIconName(const std::string& input) {
 	return str;
 }
 
+void Hook_ShowDebugMessage(
+)
+{
+	
+}
+
 /*
 		Frame Callback Event
 */
-static AurieStatus FrameCallback(FWFrame& FrameContext) {
+static AurieStatus FrameCallback(
+	IN FWFrame& FrameContext
+)
+{
 	UNREFERENCED_PARAMETER(FrameContext);
 
 	// As long as Discord API is saying it's okay, run API Callbacks
@@ -84,7 +138,10 @@ static AurieStatus FrameCallback(FWFrame& FrameContext) {
 /*
 		Code Callback Events
 */
-static AurieStatus CodeCallback(IN FWCodeEvent& CodeContext) {
+static AurieStatus CodeCallback(
+	IN FWCodeEvent& CodeContext
+)
+{
 
 	// Call original first
 	CodeContext.Call();
@@ -95,7 +152,7 @@ static AurieStatus CodeCallback(IN FWCodeEvent& CodeContext) {
 
 	// Check if CCode argument has a valid name
 	if (!Code->GetName()) {
-		g_ModuleInterface->PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to find Code Event name!");
+		PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to find Code Event name!");
 		return AURIE_MODULE_INTERNAL_ERROR;
 	}
 
@@ -136,10 +193,10 @@ static AurieStatus CodeCallback(IN FWCodeEvent& CodeContext) {
 			std::string charIconStr = ConvertToIconName(charName);
 
 			// Get the "bgmPlay" global variable - this is the index of the currently playing BGM
-			double bgmPlay = g_ModuleInterface->CallBuiltin("variable_global_get", { RValue("bgmPlay", g_ModuleInterface) }).AsReal();
+			double bgmPlay = CallBuiltin("variable_global_get", { RValue("bgmPlay", g_ModuleInterface) }).AsReal();
 
 			// Get the "gameMode" global variable - this is what game mode we're currently playing
-			double gameMode = g_ModuleInterface->CallBuiltin("variable_global_get", { RValue("gameMode", g_ModuleInterface) }).AsReal();
+			double gameMode = CallBuiltin("variable_global_get", { RValue("gameMode", g_ModuleInterface) }).AsReal();
 			std::string gameModeStr = "";
 			if (gameMode == 1) gameModeStr = " - Endless";
 			std::string stageName = stageMap[bgmPlay].first + gameModeStr;
@@ -217,7 +274,8 @@ static AurieStatus CodeCallback(IN FWCodeEvent& CodeContext) {
 EXPORTED AurieStatus ModuleInitialize(
 	IN AurieModule* Module,
 	IN const fs::path& ModulePath
-) {
+)
+{
 	UNREFERENCED_PARAMETER(ModulePath);
 
 	AurieStatus last_status = AURIE_SUCCESS;
@@ -233,7 +291,30 @@ EXPORTED AurieStatus ModuleInitialize(
 	if (!AurieSuccess(last_status))
 		return AURIE_MODULE_DEPENDENCY_NOT_RESOLVED;
 
-	g_ModuleInterface->Print(CM_LIGHTGREEN, "[DiscordRPC] - Hello from PluginEntry!");
+	Print(CM_LIGHTGREEN, "[DiscordRPC] - Hello from PluginEntry!");
+
+	
+	//// If the current game is YYC, we fire our YYC hooks
+	//if (CallBuiltin("code_is_compiled", {}).AsBool()) {
+	//	CScript* show_debug_message = nullptr;
+
+	//	// Try to get this script, it controls which options are run
+	//	last_status = g_ModuleInterface->GetNamedRoutinePointer("show_debug_message", reinterpret_cast<PVOID*>(&show_debug_message));
+
+	//	if (!AurieSuccess(last_status))
+	//		return AURIE_MODULE_INTERNAL_ERROR;
+
+	//	// Safe pointer access forever
+	//	if (show_debug_message && show_debug_message->m_Functions && show_debug_message->m_Functions->m_ScriptFunction) {
+	//		// This is how you hook YYC scripts in Next
+	//		last_status = MmCreateHook(Module, "show_debug_message", show_debug_message->m_Functions->m_ScriptFunction,	Hook_ShowDebugMessage, nullptr);
+
+	//		if (!AurieSuccess(last_status)) {
+	//			PrintError(__FILE__, __LINE__, "Failed to set script callback. Status = %d", last_status);
+	//			return AURIE_MODULE_INTERNAL_ERROR;
+	//		}
+	//	}
+	//}
 
 	// Create callback for Frame Events
 	last_status = g_ModuleInterface->CreateCallback(
@@ -244,7 +325,7 @@ EXPORTED AurieStatus ModuleInitialize(
 	);
 
 	if (!AurieSuccess(last_status)) {
-		g_ModuleInterface->PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to register callback!");
+		PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to register callback!");
 	}
 
 	// Create callback for Object Events
@@ -256,7 +337,7 @@ EXPORTED AurieStatus ModuleInitialize(
 	);
 
 	if (!AurieSuccess(last_status)) {
-		g_ModuleInterface->PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to register callback!");
+		PrintError(__FILE__, __LINE__, "[DiscordRPC] - Failed to register callback!");
 	}
 
 	/*
@@ -264,7 +345,7 @@ EXPORTED AurieStatus ModuleInitialize(
 	*/
 	result = discord::Core::Create(APPLICATION_ID, (uint64_t)discord::CreateFlags::NoRequireDiscord, &core);
 	if (result != discord::Result::Ok) {
-		g_ModuleInterface->PrintError(__FILE__, __LINE__, "Discord initialization failed: %d - is your Discord closed?", static_cast<int>(result));
+		PrintError(__FILE__, __LINE__, "Discord initialization failed: %d - is your Discord closed?", static_cast<int>(result));
 		return AURIE_SUCCESS;
 	}
 	core->SetLogHook(discord::LogLevel::Debug, LogProblemsFunction);
